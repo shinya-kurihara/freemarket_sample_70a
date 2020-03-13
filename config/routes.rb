b449165c3784a4baa04ba4d0716e33e76256489b
shinya-kurihara <jrdrp014@yahoo.co.jp>
2020-03-13 17:29:25 +0900

    add-margin-bottom

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     get 'sending_destinations', to: 'users/registrations#new_sending_destination'
     post 'sending_destinations', to: 'users/registrations#create_sending_destination'
   end
-  get 'mypages/index'
-  get 'products/index'
-  root 'items#index'
-  get 'buy/index'
+  resources :mypages, only: :index
+  resources :exhibition, only: :index
+  resources :products, only: :index
+  resources :buy, only: :index
+  root "items#index"
 end
