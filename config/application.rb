a9aca3e56d2c8b63677928939f22d398f3f99fef
shinya-kurihara <jrdrp014@yahoo.co.jp>
2020-03-20 11:50:33 +0900

    add-devise-japanese

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -17,6 +19,8 @@ module FreemarketSample70a
       g.helper false
       g.test_framework false
     end
+
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
