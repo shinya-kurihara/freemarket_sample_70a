5e353f35edc3a94ca28104b47812f232237a34d6
kuuuuuuu <gameman822@gmail.com>
2020-03-23 12:55:43 +0900

    編集完了

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,5 @@
 class ItemImage < ApplicationRecord
-  belongs_to :item, optional: true, foreign_key: :item_image_id, dependent: :destroy
+  belongs_to :item, optional: true, foreign_key: :item_image_id
   mount_uploader :image, ImageUploader
 
   # validates :image, presence: true
