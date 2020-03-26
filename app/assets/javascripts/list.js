105b3ee7fbb9267721000836e017a4d22267f9d3
daikichi1113 <happy_daikichi@yahoo.co.jp>
2020-03-26 10:19:24 +0900

    レスポンシブデザイン

---
+++ app/assets/javascripts/list.js
@@ -0,0 +1,15 @@
+// $(function(){
+//   $(".header__box__menuIcon").on("click",function(){
+//     var leftVal = 0;
+//     if ($(this).hasClass("open")){
+//       leftVal = -150;
+//       $(this).removeClass("open");
+//     } else {
+//       $(this).addClass("open");
+//     }
+
+//     $("#menuNavi").stop().animate({
+//       left: leftVal
+//     }, 200);
+//   });
+// });
